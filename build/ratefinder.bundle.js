541e2c08c753541302ad015037894be8f531aa75
jtaylor1991 <james.taylorii91@gmail.com>
2016-09-10 12:09:54 -0400

    first commit

---
+++ build/ratefinder.bundle.js
@@ -0,0 +1,65 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	var url = "rates.json";
+	
+	fetch(url).then(function (response) {
+	    return response.json();
+	}).then(function (rates) {
+	    var html = '';
+	    rates.forEach(function (rate) {
+	        return html += "<tr><td>" + rate.name + "</td><td>" + rate.years + "</td><td>" + rate.rate + "%</td></tr>";
+	    });
+	    document.getElementById("rates").innerHTML = html;
+	}).catch(function (e) {
+	    return console.log(e);
+	});
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=ratefinder.bundle.js.map
